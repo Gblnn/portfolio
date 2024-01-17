2601ee6ef265f91de703aaed716d72188429fce5
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-17 20:30:13 +0400

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -61,7 +61,7 @@ export default function Home(){
                 </div>
                 <div className="page">
                     <div style={{display:"flex", flexFlow:"column", justifyContent:"center", alignItems:"center", gap:"4rem"}}>
-                    <h1>📫 Get in touch</h1>
+                    <h1>Get in touch</h1>
                     <p style={{width:"75%", textAlign:"center"}}>Whether you are starting a project, have business inquiries or just want to say hi, my inbox is always open. So feel free to reach out and I will get back to you as soon as possible</p>
                     <button className="red btn">Reach out</button>
                     </div>
