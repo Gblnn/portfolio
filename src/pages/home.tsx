7295c2b120ec62cb06b6d72a316a5de0c0af6193
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-17 20:27:22 +0400

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -24,8 +24,12 @@ export default function Home(){
                     <div style={{display:"flex",flexFlow:"column",alignItems:"center", justifyContent:"center", border:"", gap:"1rem"}}>
                         <p style={{textAlign:"center", fontSize:"1.2rem", fontWeight:600, color:"var(--clr-slate600)"}}>Hi, I'm Gokul👋</p>
                         <h1 className="heading">FRONT-END<br/> DEVELOPER</h1>
+
+                        <div style={{width:"45ch", height:"45ch", background:"var(--nav-color)", position:"absolute",zIndex:"-1", backdropFilter:"blur(35px)"}}></div>
+
+                        <div style={{width:"20ch", height:"20ch", background:"rgba(79 70 229/75%)", position:"absolute",zIndex:"-2",borderRadius:"50%"}}></div>
                         
-                        <p style={{textAlign:"center", width:"75%"}}>A <strong>Front-end</strong>web developer and a <strong>UI/UX</strong>enthusiast</p>
+                        <p style={{textAlign:"center", width:"90%", border:""}}>A <strong>Front-end</strong>web developer and a <strong>UI/UX</strong>enthusiast</p>
                         
                     </div>
                     
