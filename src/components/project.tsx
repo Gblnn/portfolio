ebef131dcc36395d3ee0d21f0ffc9ad56dbdcae0
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-17 18:26:13 +0400

    update

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -25,24 +25,22 @@ export default function Project(props:Props) {
 
                 <div style={{width:"100%", height:"", border:"", padding:"",}}>
 
-                    <div style={{padding:"0rem", margin:"1.25rem", marginTop:"0rem", width:""}}>
+                    <div style={{padding:"0rem", margin:"1.2rem", marginTop:"0rem", width:""}}>
 
-                        <div style={{display:"flex",  justifyContent:"space-between", border:""}}>
+                        <div style={{display:"flex",  justifyContent:"space-between", border:"", gap:"0.5rem"}}>
 
                         <div style={{display:"flex",alignItems:"center", gap:"0.4rem",}}>
 
                             <a style={{textDecoration:"none", color:"var(--color)"}} href={props.link} target="_blank">
                             <h3>{props.title}</h3>
                             </a>
-                            <Link width="1.25rem"/>
+                            <Link width="1rem"/>
                             
                         </div>
 
-                        {/* <button style={{padding:"0.25rem", paddingLeft:"0.75rem", paddingRight:"0.75rem"}} className="dark btn"><a href={props.github}></a>Github</button> */}
-                        
-                        
+                            <button style={{fontSize:"0.8rem",marginRight:"0rem",padding:"0.2rem", paddingLeft:"0.75rem", paddingRight:"0.75rem"}} className="dark btn"><a href={props.github}></a>Github</button>
                         </div>
-                        <p style={{marginTop:"0.5rem", fontSize:"0.85rem"}}>{props.desc}</p>
+                        {/* <p style={{marginTop:"0.5rem", fontSize:"0.85rem"}}>{props.desc}</p> */}
                     
                     </div>
                     
