e4f4f2c7e650a5753171b19ad6fea1045f50a9d8
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-17 22:05:46 +0400

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -21,39 +21,47 @@ export default function Home(){
                 </div>
 
                 <div id="work" className="page" style={{border:"",width:"100%", height:"fit-content"}}>
-                    <div style={{height:"4rem"}}></div>
-                    <div className="page-container" style={{border:"", width:"100%", height:"100%"}}>
 
-                    <div style={{border:"", marginTop:"3.5rem", marginBottom:"2.5rem"}}>
+                    <div style={{height:"4rem"}}></div>
+
+                    <div className="page-container" style={{width:"100%", height:"100%"}}>
+
+                        <div style={{marginTop:"3.5rem", marginBottom:"2.5rem"}}>
                             <h2 style={{textAlign:"center"}}>💼 Work</h2>
                         </div>
+
                     </div>
                         
 
-                        <div className="project-container" style={{marginTop:"1rem"}}>
-                            <Project img="/coffee.png" title="Coffee" link="https://coffee-dark.netlify.app" desc="Coffee is a textual social media app written in React and TypeScript"/>
+                    <div className="project-container" style={{marginTop:"1rem"}}>
 
-                            <Project img="/university-page.png" title="Landing Page" link="https://hw-page.netlify.app" desc="A recreation of HW university landing page written in React and TypeScript"/>
+                        <Project img="/coffee.png" title="Coffee" link="https://coffee-dark.netlify.app" desc="Coffee is a textual social media app written in React and TypeScript"/>
 
-                            <Project img="/dashboard.png" title="Dashboard" link="https://dashboard-nine.netlify.app" desc="A highly responsive dashboard design written in React and TypeScript"/>
+                        <Project img="/university-page.png" title="Landing Page" link="https://hw-page.netlify.app" desc="A recreation of HW university landing page written in React and TypeScript"/>
 
-                            <Project img="/blaze-page.png" title="Blaze" desc="An online store design written in React and TypeScript with search filter"/>
+                        <Project img="/dashboard.png" title="Dashboard" link="https://dashboard-nine.netlify.app" desc="A highly responsive dashboard design written in React and TypeScript"/>
+
+                        <Project img="/blaze-page.png" title="Blaze" desc="An online store design written in React and TypeScript with search filter"/>
                             
-                        </div>
-                        
-                    
-                    
-                </div>
-                <div className="page">
-                    <div style={{display:"flex", flexFlow:"column", justifyContent:"center", alignItems:"center", gap:"4rem"}}>
-                    <h1 id="contact">Get in touch</h1>
-                    <p className="desc" style={{ textAlign:"center"}}>Whether you are starting a project, have business inquiries or just want to say hi, my inbox is always open. So feel free to reach out and I will get back to you as soon as possible</p>
-                    <div style={{display:"flex", gap:"1.5rem"}}>
-                    <a href="https://github.com/Gblnn" target="_blank"><Github/></a>
-                    <a href="https://linkedin.com/in/gokulnathiel" target="_blank"><Linkedin/></a>
                     </div>
+                        
+                </div>
+
+                <div className="page">
+
+                    <div style={{display:"flex", flexFlow:"column", justifyContent:"center", alignItems:"center", gap:"2rem"}}>
+
+                        <h1 id="contact">Get in touch</h1>
+
+                        <p className="desc" style={{ textAlign:"center"}}>Whether you are starting a project, have business inquiries or just want to say hi, my inbox is always open. So feel free to reach out and I will get back to you as soon as possible</p>
+
+                        <div style={{display:"flex", gap:"1.5rem"}}>
+                            <a href="https://github.com/Gblnn" target="_blank"><Github/></a>
+                            <a href="https://linkedin.com/in/gokulnathiel" target="_blank"><Linkedin/></a>
+                        </div>
                     
-                    <button className="red btn">Reach out</button>
+                        <button className="red btn">Reach out</button>
+
                     </div>
                     
                 </div>
