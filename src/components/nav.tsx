cf1d2eda551e9035458e251751ed24ae13c31614
Gokul <122973682+Gblnn@users.noreply.github.com>
2024-01-28 17:53:25 +0400

    Update nav.tsx

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -9,7 +9,7 @@ export default function Nav(){
                 <a href="#work">Work</a>
                 <a href="#contact">Contact</a>
             </div>
-            <button style={{marginRight:"2rem", marginLeft:"2rem"}} className="blue btn"><a style={{color:"white"}} href="https://drive.google.com/file/d/1cb5womo-Zku-TETW-YtUFG2-NQFXfNml/view?usp=sharing">📄 Resume</a></button>
+            <button style={{marginRight:"2rem", marginLeft:"2rem"}} className="blue btn"><a style={{color:"white"}} href="https://drive.google.com/file/d/1cb5womo-Zku-TETW-YtUFG2-NQFXfNml/view?usp=sharing" target="_blank">📄 Resume</a></button>
         </div>
         
     )
